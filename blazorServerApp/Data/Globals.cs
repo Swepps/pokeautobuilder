7b17569f00bbf386ceed4fe0e9c8b2fe597fd5a3
georgeo <georgeo@pulsonix.com>
2023-06-27 15:24:19 +0100

    completely reshuffled repositry since we had two obsolete projects in here. They have been moved to 'archive' and the main WASM project is now in the root dir.

--- blazorServerApp/Data/Globals.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace blazorServerApp.Data
-{
-	public class Globals
-	{
-		public static string Language = "en";
-		public static List<SmartType> AllTypes = new List<SmartType>();
-		public static SmartPokedex Pokedex = new SmartPokedex();
-	}
-}
