e45f17a7a4187b8d3e365e797d4c0ff926c78376
George Osgood <georgeo@pulsonix.com>
2024-06-15 11:14:13 +0100

    Add AutoBuilder project
    Moves team generation stuff into own package

---
+++ AutoBuilder/PokemonTeamFitness.cs
@@ -0,0 +1,28 @@
+﻿using GeneticSharp;
+using PokemonDataModel;
+
+namespace AutoBuilder
+{
+    public class PokemonTeamFitness : IFitness
+    {
+        private readonly AutoBuilderWeightings _weightings;
+
+        public PokemonTeamFitness(AutoBuilderWeightings weightings)
+        {
+            _weightings = weightings;
+        }
+
+        public double Evaluate(IChromosome chromosome)
+        {
+            PokemonTeam team = (chromosome as PokemonTeamChromosome)!.GetTeam();
+
+            double fitness = AutoBuilder.CalculateScore(team, _weightings);
+
+            // duplicates in a team don't qualify as a valid team
+            if (fitness < 0 || team.ContainsDuplicates())
+                fitness = 0;
+
+            return fitness;
+        }
+    }
+}
